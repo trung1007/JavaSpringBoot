6dc830fe75861350aee4d491956d5fb49e95c3f2
trung1007 <trungthanhcao.2003@gmail.com>
2024-07-28 13:48:30 +0700

    building first API

---
+++ src/main/java/CaoTrung/springProject/student/StudentController.java
@@ -0,0 +1,19 @@
+package CaoTrung.springProject.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+    @GetMapping
+    public List<String> findAllStudents(){
+        return  List.of(
+                "Cao Trung",
+                "Hello World"
+        );
+    }
+}
